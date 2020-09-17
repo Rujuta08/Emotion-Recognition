a84aede9a93427a971a24decbfc33657ee245e8f
Rujuta08 <64728512+Rujuta08@users.noreply.github.com>
2020-09-17 18:32:19 +0530

    Update face_emotion_detection.py
    Unnecessary comments removed

--- face_emotion_detection.py
+++ face_emotion_detection.py
@@ -7,13 +7,11 @@ import numpy as np
 
 # parameters for loading data and images
 detection_model_path = 'haarcascade_frontalface_default.xml'
-#emotion_model_path = '_mini_XCEPTION.106-0.65.hdf5'
 emotion_model_path = 'model.h5'
 
 # hyper-parameters for bounding boxes shape
 # loading models
 face_detection = cv2.CascadeClassifier(detection_model_path)
-#emotion_classifier = load_model(emotion_model_path, compile=False)
 model = model_from_json(open("model.json","r").read())
 model.load_weights('model.h5')
 EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
@@ -36,16 +34,15 @@ while True:
         faces = sorted(faces, reverse=True,
         key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
         (fX, fY, fW, fH) = faces
-                    # Extract the ROI of the face from the grayscale image, resize it to a fixed 48x48 pixels, and then prepare
-            # the ROI for classification via the CNN
+        # Extract the ROI of the face from the grayscale image, resize it to a fixed 48x48 pixels, and then prepare
+        # the ROI for classification via the CNN
         roi = gray[fY:fY + fH, fX:fX + fW]
         roi = cv2.resize(roi, (48, 48))
         roi = roi.astype("float") / 255.0
         roi = img_to_array(roi)
         roi = np.expand_dims(roi, axis=0)
         
-        
-        #preds = emotion_classifier.predict(roi)[0]
+       
         preds = model.predict(roi)[0]
         emotion_probability = np.max(preds)
         label = EMOTIONS[preds.argmax()]
