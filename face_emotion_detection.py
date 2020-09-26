1be3ea9928b3022758b36dec4678a45ec4d1aa62
Rujuta08 <vaze.rujuta@gmail.com>
2020-09-26 15:16:19 +0530

    final Updation

--- face_emotion_detection.py
+++ face_emotion_detection.py
@@ -2,29 +2,27 @@
 import imutils
 import cv2
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import model_from_json
+from tensorflow.keras.models import load_model
 import numpy as np
 
 # parameters for loading data and images
 detection_model_path = 'haarcascade_frontalface_default.xml'
-emotion_model_path = 'model.h5'
+emotion_model_path = 'Model.h5'
 
 # hyper-parameters for bounding boxes shape
 # loading models
 face_detection = cv2.CascadeClassifier(detection_model_path)
-model = model_from_json(open("model.json","r").read())
-model.load_weights('model.h5')
-EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
- "neutral"]
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["angry" , "scared", "happy", "sad", "surprised","neutral"]
 
 
 # starting video streaming
-cv2.namedWindow('your_face')
+cv2.namedWindow('Face')
 camera = cv2.VideoCapture(0)
 while True:
     frame = camera.read()[1]
     #reading the frame
-    frame = imutils.resize(frame,width=400)
+    frame = imutils.resize(frame,width=600)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
     
@@ -34,18 +32,20 @@ while True:
         faces = sorted(faces, reverse=True,
         key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
         (fX, fY, fW, fH) = faces
-        # Extract the ROI of the face from the grayscale image, resize it to a fixed 48x48 pixels, and then prepare
-        # the ROI for classification via the CNN
+        #Extract the ROI of the face from the grayscale image,
+        #resize it to a fixed 48x48 pixels, and then prepare
+        #the ROI for classification via the CNN
         roi = gray[fY:fY + fH, fX:fX + fW]
         roi = cv2.resize(roi, (48, 48))
-        roi = roi.astype("float") / 255.0
+        roi = roi.astype("float")
         roi = img_to_array(roi)
         roi = np.expand_dims(roi, axis=0)
         
-       
-        preds = model.predict(roi)[0]
+        
+        preds = emotion_classifier.predict(roi)[0]
         emotion_probability = np.max(preds)
         label = EMOTIONS[preds.argmax()]
+
 
  
         for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
@@ -62,10 +62,14 @@ while True:
                     cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
                                   (0, 0, 255), 2)
 
-    cv2.imshow('your_face', frameClone)
-    cv2.imshow("Probabilities", canvas)
+    cv2.imshow('Face', frameClone)
+    cv2.imshow("Emotion Probabilities", canvas)
+    #Code terminates on pressing key 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 camera.release()
 cv2.destroyAllWindows()
+
+
+
